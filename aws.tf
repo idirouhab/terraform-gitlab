61fd2db8f861d8b96644a6146bcde864b642b222
Idir Ouhab Meskine <idirouhab@Idirs-MBP.home>
2022-08-08 12:54:47 +0200

    Initial commit

---
+++ aws.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.25.0"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_iam_user" "new_user" {
+  name = "DemoUser"
+}
+
+resource "aws_iam_access_key" "AccK" {
+  user = aws_iam_user.new_user.name
+}
+
+output "secret_key" {
+  value = aws_iam_access_key.AccK.secret
+  sensitive = true
+}
+
+output "access_key" {
+  value = aws_iam_access_key.AccK.id
+}
