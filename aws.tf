6221b7cddde574988b0d6c14567c2741c3b7f4b7
Idir Ouhab Meskine <iouhab@gitlab.com>
2022-08-09 11:17:08 +0200

    Add vars.tf

--- aws.tf
+++ aws.tf
@@ -12,9 +12,9 @@ terraform {
 
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+  region     = "${var.REGION}"
 }
 
 resource "aws_iam_user" "new_user" {
